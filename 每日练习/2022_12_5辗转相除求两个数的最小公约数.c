803d397c9c4887a54182b561dbbc8aac2062917b
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-05 20:28:00 +0800

    Add files via upload

---
+++ 每日练习/2022_12_5辗转相除求两个数的最小公约数.c
@@ -0,0 +1,18 @@
+//辗转相除法 求最小公约数
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 4;
+	int b = 6;
+	int temp;
+	scanf("%d %d",&a,&b);
+	while (b % a!=0)
+	{
+		temp = b % a;
+		b = a;
+		a = temp;
+	}
+	printf("%d\n",a);
+	return 0;
+}
