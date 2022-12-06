bc63f7e0692bbf640016ba7c3da066625f16425d
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-06 08:49:18 +0800

    Add files via upload

--- 每日练习/2022_12_6.c
+++ 每日练习/2022_12_6.c
@@ -4,20 +4,14 @@ int main()
 {
 	int count = 0;
 	int year;
-	for (year = 2000; year <= 2025; year++)
+	for (year = 1000; year <= 2000; year++)
 	{
-		if (year % 4 == 0 && year % 100 != 0)
+		if ((year % 4 == 0 && year % 100 != 0)|| year % 400 == 0)
 		{
 			printf("%d  ", year);
 			count++;
 		}
-		else if (year % 400 == 0)
-		{
-			printf("%d  ",year);
-			count++;
-		}
 	}
-	
 	printf("\ncount=%d \n",count);
 	return 0;
 }
