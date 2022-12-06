abcee08f7bbf276b7c90c346745faa30dcb01665
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-06 08:29:10 +0800

    Add files via upload

---
+++ 每日练习/2022_12_6.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	int year;
+	for (year = 2000; year <= 2025; year++)
+	{
+		if (year % 4 == 0 && year % 100 != 0)
+		{
+			printf("%d  ", year);
+			count++;
+		}
+		else if (year % 400 == 0)
+		{
+			printf("%d  ",year);
+		}
+	}
+	
+	printf("\ncount=%d \n",count);
+	return 0;
+}
