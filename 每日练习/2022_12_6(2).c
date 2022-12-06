f476a9b47ff75a2bc3a67ece594daf551f38d138
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-06 18:42:57 +0800

    Add files via upload

---
+++ 每日练习/2022_12_6(2).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,count=0;
+	for (i = 10; i <= 100; i++)
+	{
+		for (j = 2; j <= i; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (i == j)
+		{
+			count++;
+			printf("%d ",i);
+		}
+	}
+	printf("\ncount = % d",count);
+	return 0;
+}
