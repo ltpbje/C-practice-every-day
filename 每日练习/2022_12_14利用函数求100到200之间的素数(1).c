37e1835108b02fb17f8e910f1d998bf7fde0f796
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-14 20:41:47 +0800

    Add files via upload

---
+++ 每日练习/2022_12_14利用函数求100到200之间的素数(1).c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+int is_prime(int a)
+{
+	int i;
+	for (i = 2; i <= sqrt(a); i++)
+	{
+		if (a % i == 0)
+			return 0;
+	}
+	return 1;
+}
+int main(void)
+{
+	int a,n;
+	for (a = 100; a <= 200; a++)
+	{
+		if (is_prime(a) == 1)
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return 0;
+}
