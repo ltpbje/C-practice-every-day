ba4a64d3c918f865d8befc98a6c58ff34f176dad
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-15 18:04:14 +0800

    Add files via upload

---
+++ 每日练习/2022_12_15利用函数的二分查找.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+int binary_search(int arr[], int k, int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while(left<=right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else 
+		{
+			return mid;
+		}
+	}
+	return 0;
+}
+int main(void)
+{ 
+	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+	int k = 5;
+	int sz;
+	sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = binary_search(arr,k,sz);
+	if (ret!=0)
+		printf("找到了下标为%d\n",ret);
+	else if (ret == 0)
+		printf("找不到\n");
+	return 0;
+}
