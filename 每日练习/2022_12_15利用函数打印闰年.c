bb18872e50a639396d7daded66cdb624a8856f6c
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-15 09:44:29 +0800

    Add files via upload

---
+++ 每日练习/2022_12_15利用函数打印闰年.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+int is_leapyear(int year)
+{
+	if ((year % 4 == 0 && year % 100 == 0) || (year % 400 == 0))
+		return 1;
+}
+int main(void)
+{
+	int year = 0;
+	for (year = 1000; year < 2000; year++)
+	{
+		if (is_leapyear(year) == 1)
+			printf("%d ",year);
+	}
+	printf("\n");
+	return 0;
+}
