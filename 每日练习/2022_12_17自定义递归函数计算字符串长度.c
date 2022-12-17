23dc349a207d626801eae1b7653c4c08397e2c4e
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-17 15:33:25 +0800

    Add files via upload

---
+++ 每日练习/2022_12_17自定义递归函数计算字符串长度.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int my_strlen(char* arr)
+{
+	if (*arr != '\0')
+	{
+		return (1 + my_strlen(arr+1));
+	}
+	else
+		return 0;
+}
+int main(void)
+{ 
+	char arr[] = "abc";
+	int len = my_strlen(arr);
+	printf("%d\n",len);
+	return 0;
+}
