ab4138a91466f2870d2814a53c8cc187f62a80f9
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-17 15:14:41 +0800

    Add files via upload

---
+++ 每日练习/2022_12_17自定义函数计算字符串长度.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int my_strlen(char* arr)
+{
+	int count = 0;
+	while (*arr != '\0')
+	{
+		count++;
+		arr++;
+	}
+	return count;
+}
+int main(void)
+{ 
+	char arr[] = "abc";
+	int len = my_strlen(arr);
+	printf("%d\n",len);
+	return 0;
+}
