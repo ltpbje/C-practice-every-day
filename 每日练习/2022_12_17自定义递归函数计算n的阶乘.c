de261ccd61e8eb8923aacf6f511e6fa3f79bb5e4
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-17 16:39:47 +0800

    Add files via upload

---
+++ 每日练习/2022_12_17自定义递归函数计算n的阶乘.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int Fac(int n)
+{
+	if (n <= 1)
+		return 1;
+	else
+		return n * Fac(n - 1);
+}
+int main(void)
+{ 
+	int n = 0;
+	scanf("%d",&n);
+	int ret = Fac(n);
+	printf("ret=%d\n",ret);
+	return 0;
+}
