3d227f8d1a92438d9fb361e0e668a9291e79036a
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-18 11:25:50 +0800

    Add files via upload

---
+++ 每日练习/2022_12_18利用递归求第n个斐波那契数.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int Fib(int n)
+{
+	if (n <= 2)
+		return 1;
+	else
+		return Fib(n-1)+Fib(n-2);
+}
+int main(void)
+{ 
+	int n = 0;
+	scanf("%d",&n);
+	int fib = Fib(n);
+	printf("%d\n",fib);
+	return 0;
+}
