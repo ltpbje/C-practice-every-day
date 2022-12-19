4c4e6575d8abf1dd2d288312f13f0c6faaf7f03b
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-19 22:06:00 +0800

    Add files via upload

---
+++ 每日练习/2022_12_19颠倒字符串输出.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int is_strlen(char* arr)
+{
+	int count = 0;
+	while (*arr != '\0')
+	{
+		arr++;
+		count++;
+	}
+	return count;
+}
+void reverse_arr(char* arr)
+{
+	int left = 0;
+	int right = is_strlen(arr) - 1;
+	while (left < right)
+	{
+		char temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
+		left++;
+		right--;
+	}
+}
+int main(void)
+{ 
+	char arr[] = "abcdefg";
+	reverse_arr(arr);
+	printf("%s\n",arr);
+	return 0;
+}
