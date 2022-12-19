8a7872dfa707866ac3d5005ac0e231e7c509affc
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-19 14:52:49 +0800

    Add files via upload

---
+++ 每日练习/2022_12_19打印nn乘法表.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+void print_table(int n)
+{
+	int i = 0;
+	for (i = 1; i <= n; i++)
+	{
+		int j = 0;
+		for (j = 1; j <= i; j++)
+			printf("%-2d*%-2d=%-3d ",i,j,i*j);
+		printf("\n");
+	}
+}
+int main(void)
+{ 
+	int n = 0;
+	scanf("%d",&n);
+	print_table(n);
+	return 0;
+}
