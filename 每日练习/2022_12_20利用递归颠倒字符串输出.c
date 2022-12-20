b2b2a6fb1926bff697f5bf07c827d49b565c2b83
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-20 19:48:49 +0800

    Add files via upload

---
+++ 每日练习/2022_12_20利用递归颠倒字符串输出.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int is_strlen(char* arr)
+{
+	int count = 0;
+	while (*arr != '\0')
+	{
+		arr++;
+		count++;
+	}
+	return count;
+}
+void reverse_arr(char* arr)
+{
+	char temp = *arr;
+	int len = is_strlen(arr);
+	*arr = *(arr+len-1);
+	*(arr + len - 1) = '\0';
+	if (is_strlen(arr+1)>=2)
+	{
+		reverse_arr(arr+1);
+	}
+	*(arr + len - 1) = temp;
+}
+int main(void)
+{ 
+	char arr[] = "abcdefg";
+	reverse_arr(arr);
+	printf("%s\n",arr);
+	return 0;
+}
