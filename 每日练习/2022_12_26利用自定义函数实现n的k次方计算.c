33ab493de4e2faf86e2de684edac6cbe505a3bb8
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-26 21:41:41 +0800

    Add files via upload

---
+++ 每日练习/2022_12_26利用自定义函数实现n的k次方计算.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int Pow(int n,int k)
+{
+	int i = 1;
+	int sum = 1;
+	for (i = 1; i <= k; i++)
+	{
+		sum *= n;
+	}
+	return sum;
+}
+int main(void)
+{ 
+	int n, k;
+	scanf("%d%d",&n,&k);
+	int ret = Pow(n,k);
+	printf("%d\n",ret);
+	return 0;
+}
