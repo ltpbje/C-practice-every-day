4d361a7032a0b6471cb2afead7bf9f308915f4db
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-26 21:32:03 +0800

    Add files via upload

---
+++ 每日练习/2022_12_26利用递归输出一个数的每位数字之和(鹏哥的方法).c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int DigitSum(int num)
+{
+	if (num > 9)
+		return DigitSum(num / 10) + (num % 10);
+	else
+		return num;
+}
+int main(void)
+{ 
+	int num = 1729;
+	int sum = DigitSum(num);
+	printf("%d\n",sum);
+	return 0;
+}
