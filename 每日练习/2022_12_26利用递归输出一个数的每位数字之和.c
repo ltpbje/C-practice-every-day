4ce35a7d545562981736758d4fd17373b7bac293
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-26 21:16:00 +0800

    Add files via upload

---
+++ 每日练习/2022_12_26利用递归输出一个数的每位数字之和.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int DigitSum(int num)
+{
+	int a = num % 10;
+	num = num / 10;
+	int sum = a;
+	if (num != 0) 
+	{
+		 sum = DigitSum(num) + a;
+	}
+	return sum;
+}
+int main(void)
+{ 
+	int num = 1729;
+	int sum = DigitSum(num);
+	printf("%d\n",sum);
+	return 0;
+}
