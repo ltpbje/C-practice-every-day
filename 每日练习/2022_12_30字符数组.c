b8c9823a8a95cb705c4b055a3a8b9eb7228066c6
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2022-12-30 19:55:41 +0800

    Add files via upload

---
+++ 每日练习/2022_12_30字符数组.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = {"abc"};
+	char arr2[] = { 'a' ,'b','c'};
+	printf("%s\n",arr1);
+	printf("%s\n",arr2);
+	printf("%d\n",strlen(arr1));
+	printf("%d\n",strlen(arr2));
+	return 0;
+}
