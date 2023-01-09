d5e5474390397d1a03305cf8650eea2c0c9e6edf
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2023-01-09 20:34:21 +0800

    Add files via upload

--- 每日练习/2023_1_4三子棋游戏/game.c
+++ 每日练习/2023_1_4三子棋游戏/game.c
@@ -20,16 +20,22 @@ void DisplayBoard(char board[ROW][COL], int row, int col)
 	{
 		int j = 0;
 		for (j = 0; j < col; j++)
-		{
-			
-			
+		{	
 			printf("%c", board[i][j]);
-			if (j < col - 1)	
-			{
-				printf("--| ");
-			}
-			if(i<row-1)
-			printf("--");
+			if(j < col - 1)
+			printf("  |");
 		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			j = 0;
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j < col - 1)
+					printf("|");
+			}
+		}
+		printf("\n");
 	}
 }
--- 每日练习/2023_1_4三子棋游戏/game.h
+++ 每日练习/2023_1_4三子棋游戏/game.h
@@ -1,7 +1,7 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
-#define ROW 3
-#define COL 3
+#define ROW 5
+#define COL 5
 void Initboard(char board[ROW][COL], int row, int col);
 void DisplayBoard(char board[ROW][COL], int row, int col);
 
