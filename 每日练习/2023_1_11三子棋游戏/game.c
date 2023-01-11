d51b44b59688beeaea99a9fe8df3b8e4d17bf9be
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2023-01-11 21:14:04 +0800

    Add files via upload

---
+++ 每日练习/2023_1_11三子棋游戏/game.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"game.h"
+void Initboard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col - 1; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+
+}
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			printf("%c", board[i][j]);
+			if (j < col - 1)
+				printf("  |");
+		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			j = 0;
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j < col - 1)
+					printf("|");
+			}
+		}
+		printf("\n");
+	}
+}
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	while (1)
+	{
+		printf("玩家走>\n");
+		printf("请选择所下位置>:");
+		scanf("%d %d", &x, &y);
+		if (x <= row && y <= col && x >= 1 && y >= 1)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("被占用了,重新输入>");
+			}
+		}
+		else
+		{
+			printf("选择错误,重新选择》");
+		}
+	}
+}
---
+++ 每日练习/2023_1_11三子棋游戏/test.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include"game.h"
+void menu()
+{
+	printf("*************************\n");
+	printf("********1.play***********\n");
+	printf("********0.exit***********\n");
+	printf("*************************\n");
+}
+void game()
+{
+	//储存二维数组
+	char board[ROW][COL];
+	//初始化二维数组
+	Initboard(board, ROW, COL);
+	DisplayBoard(board, ROW, COL);
+	while (1)
+	{
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+	}
+}
+int main()
+{
+	int input;
+	do
+	{
+		menu();
+		printf("请选择:>>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("三子棋游戏\n");
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，重新选择\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
