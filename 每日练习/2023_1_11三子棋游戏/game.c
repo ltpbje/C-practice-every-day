d738b537689ced397652acac3f976110cf426136
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2023-01-14 22:14:58 +0800

    Add files via upload

--- 每日练习/2023_1_11三子棋游戏/game.c
+++ 每日练习/2023_1_11三子棋游戏/game.c
@@ -84,5 +84,16 @@ void ComputerMove(char board[ROW][COL], int row, int col)
 }
 char isWin(char board[ROW][COL], int row, int col)
 {
-	
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			if(board[][]=)
+
+		}
+		if ()
+	}
+		
 }
