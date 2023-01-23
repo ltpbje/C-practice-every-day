cd233dc9d202536ef45b2edd5683cce7a580ea30
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2023-01-23 16:55:02 +0800

    Add files via upload

---
+++ 每日练习/2023_1_23扫雷/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void menu()
+{
+	printf("********************************\n");
+	printf("***********1.Play***************\n");
+	printf("***********0.Exit***************\n");
+	printf("********************************\n");
+}
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择>>:");
+		scanf("%d",&input);
+		switch (input)
+		{
+		case 1:
+			printf("扫雷游戏\n");
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入错误,重新输入\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
