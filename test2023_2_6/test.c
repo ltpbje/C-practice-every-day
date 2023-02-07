8a173f74d0bc502b014e94b5d14cd281fd89f01d
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2023-02-07 20:56:56 +0800

    Add files via upload

---
+++ test2023_2_6/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+//{
+//	float a = 2.0f / 5.0f;
+//	printf("%f\n",a);
+//	return 0;
+//}
+//{
+//	int a = 2 % 3;
+//	printf("a=%d\n",a);
+//	return 0;
+//}
+//{
+//	int a = 2;
+//	int b = a << 1;//把a的二进制位向左移动一位
+//	printf("%d\n",b);
+//	return 0;
+//}
+{
+	int a = -1;
+	int b = a >> 1;
+	printf("%d\n",b);
+	return 0;
+}
