46305e10204b752afbf29d2a625229668c12fdc4
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2023-02-09 22:41:38 +0800

    Add files via upload

--- 每日练习/test2023_2_6/test.c
+++ 每日练习/test2023_2_6/test.c
@@ -11,9 +11,43 @@ int main()
 //	printf("a=%d\n",a);
 //	return 0;
 //}
+//{
+//	int a = 2;
+//	int b = a << 1;//把a的二进制位向左移动一位
+//	printf("%d\n",b);
+//	return 0;
+//}
+//{
+//	int a = -1;
+//	int b = a >> 1;
+//	printf("%d\n",b);
+//	return 0;
+//}
+//{
+//	int a = 2;
+//	int b = 1;
+//	printf("%d\n",a^b);
+//}
+//不创建变量来交换两个变量的值
+//{
+//	int a = 2;
+//	int b = 3;
+//	a = a + b;
+//	b = a - b;
+//	a = a - b;
+//	printf("%d %d\n",a,b);
+//}
+//{
+//	int a = 2;
+//	int b = 3;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a=%d b=%d\n", a, b);
+//}
 {
-	int a = 2;
-	int b = a << 1;//把a的二进制位向左移动一位
-	printf("%d\n",b);
+	int a = 13;
+	a = a | (1 << 4);
+	printf("%d\n",a);
 	return 0;
 }
