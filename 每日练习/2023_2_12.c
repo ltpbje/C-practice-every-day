3ee34484685dbad631b863feba17e6547fc35655
乐天居易 <118698369+ltpdbz@users.noreply.github.com>
2023-02-12 19:42:17 +0800

    Add files via upload

--- 每日练习/2023_2_12.c
+++ 每日练习/2023_2_12.c
@@ -1,26 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int judge(int i)
+//{
+//	if (i % 3 == 0 && i % 5 == 0)
+//	{
+//		return 1;
+//	}
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i=15;i<=300;i++) 
+//	{
+//		int	m=judge(i);
+//		if (m == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d\n",count);
+//	return 0;
+//}
 #include<stdio.h>
-int judge(int i)
+#include<math.h>
+int main()
+void fun(int m)
 {
-	if (i % 3 == 0 && i % 5 == 0)
+	int i = m;
+	int j = 0;
+	int sum = 0;
+	while (i)
 	{
-		return 1;
+		i = i / 10;
+		j = j % 10;
+		sum += pow(j,3);
+	}
+	if (sum == i)
+	{
+		printf("是水仙花数\n");
 	}
 	else
-		return 0;
 }
-int main()
 {
-	int count = 0;
 	int i = 0;
-	for (i=15;i<=300;i++) 
-	{
-		int	m=judge(i);
-		if (m == 1)
-		{
-			count++;
-		}
-	}
-	printf("%d\n",count);
+
+	fun(i);
 	return 0;
 }
