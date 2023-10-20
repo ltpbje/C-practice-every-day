9bffeaf33c0e8504728bbf34e333ed531d91d08e
乐天居易 <118698369+ltpbje@users.noreply.github.com>
2023-10-20 21:18:52 +0800

    Update 08-舞伴问题.cpp

--- 数据结构实验作业/08-舞伴问题.cpp
+++ 数据结构实验作业/08-舞伴问题.cpp
@@ -29,13 +29,13 @@ typedef struct
 }Person;
 //----------队列的顺序存储结构---------
 #define MAXQSIZE 100          //队列可能达到的最大长度
-typedef struct			
+typedef struct
 {
 	Person* base;			  //队列中数据元素类型为Person
 	int front;				  //头指针
 	int rear;				  //尾指针
 }SqQueue;
-Status InitQueue(SqQueue &Q)
+Status InitQueue(SqQueue& Q)
 {//构造一个空队列Q
 	Q.base = new Person[MAXQSIZE];
 	if (!Q.base) exit(OVERFLOW);
@@ -61,21 +61,72 @@ int QueueEmpty(SqQueue Q)
 		return 0;
 	}
 };
-void DancerPartner(Person dancer[],int num)
+Status DeQueue(SqQueue& Q, Person& e)
+{//删除Q的队头元素,用e返回其值
+	if (Q.front == Q.rear) return ERROR;  //队空
+	e = Q.base[Q.front];
+	Q.front = (Q.front + 1) % MAXSIZE;
+	return OK;
+}
+
+Person GetHead(SqQueue Q) 
+{	
+	if (Q.front != Q.rear)
+		return Q.base[Q.front];
+}
+void DancePartner(Person dancer[], int num)
 {
+	int n = 0;
+	printf("要进行的轮数:");
+	scanf("%d",&n);
 	SqQueue Mdancers, Fdancers;	 //分别存放男士和女士入队者
 	InitQueue(Mdancers);
 	InitQueue(Fdancers);
-	//结构数组dancer中存放跳舞的男女姓名和性别，num是跳舞的人数
-	for (int i = 0; i < num; i++)
+	Person p;
+	for(int j =0;j<n;j++)
 	{
-		Person p = dancer[i];
-		if (p.sex == 'F') EnQueue(Fdancers, p);
-		else EnQueue(Mdancers, p);
-	};
-	printf("The dancing partners are:\n");
-	while (!QueueEmpty(Fdancers) && !QueueEmpty(Mdancers))
-	{
-
+		printf("-------第%d轮--------\n",j+1);
+		//结构数组dancer中存放跳舞的男女姓名和性别，num是跳舞的人数
+		for (int i = 0; i < num; i++)
+		{
+			p = dancer[i];
+			if (p.sex == 'F') EnQueue(Fdancers, p);
+			else EnQueue(Mdancers, p);
+		};
+		printf("舞伴是:\n");
+		while (!QueueEmpty(Fdancers) && !QueueEmpty(Mdancers))
+		{
+			DeQueue(Fdancers, p); //女性出队
+			printf("%s ", p.name);//输出女性的姓名
+			DeQueue(Mdancers, p); //男性出队
+			printf("%s\n", p.name);//输出男性的姓名
+		};
+		if (!QueueEmpty(Fdancers))
+		{
+			p = GetHead(Fdancers);
+			printf("第一个得到舞伴的女性: %s\n\n", p.name);
+		}
+		else if (!QueueEmpty(Mdancers))
+		{
+			p = GetHead(Mdancers);
+			printf("第一个得到舞伴的男性: %s\n\n", p.name);
+		}
 	}
 };
+int main() {
+	Person dancer[MAXSIZE]; int i, num;
+	printf("请输入跳舞总人数：");
+	scanf("%d", &num);
+	printf("请输入各个跳舞人的姓名和性别（'F'表示女性，'M'表示男性）：\n");
+	for (i = 0; i < num; i++)
+	{
+		printf("请输入第%d个跳舞人的姓名和性别（用空格隔开）：", i + 1);
+		scanf("%s %c", &dancer[i].name, &dancer[i].sex);
+	};
+	
+	DancePartner(dancer, num);
+	return 0;
+}
+
+
+
